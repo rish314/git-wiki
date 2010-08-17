3c797803f58de34d7cbf77cffc990017f3360963
Tijn Schuurmans <tijn@soocial.com>
2010-08-18 01:39:47 +0200

    making the links like wikipedia

--- extensions.rb
+++ extensions.rb
@@ -2,7 +2,7 @@ def require_gem_with_feedback(gem)
   begin
     require gem
   rescue LoadError
-    puts "You need to 'sudo gem install #{gem}' before we can proceed"
+    puts "You need to 'gem install #{gem}' before we can proceed"
   end
 end
 
@@ -10,11 +10,10 @@ end
 
 class String
 
-  # matches [Page] or [[Page]] or even [[a page]]
-  SIMPLE_WIKI_LINK = /\[{1,2}([\w\s\+\-\_]+)\]{1,2}/
-  # matches [a description of Page|Page] or [[blah blah|Page]]
-  COMPLEX_WIKI_LINK = /\[{1,2}([\w\s]+)\|([\w\s]+)\]{1,2}/
-
+  # matches [[Page]] or even [[a page]] just like in wikipedia and gollum
+  SIMPLE_WIKI_LINK = /\[\[([\w\s\+\-\_]+)\]\]/
+  # matches [[Texas|Lone Star state]] just like wikipedia and unlike gollum (gollum reverses the order of things)
+  COMPLEX_WIKI_LINK = /\[\[([\w\s]+)\|([\w\s]+)\]\]/
   def wiki_linked
 #     self.gsub!(/(?!<nowiki>)(?>\b((?:[A-Z]\w+){2,}))(?!<\/nowiki>)/) { |m| "<a href=\"/#{m}\">#{m}</a>" }
 #     self.gsub!(/<\/?nowiki>/,'')
@@ -23,7 +22,7 @@ class String
       '<a href="/%s">%s</a>' % [s.as_wiki_link, s]
     end
     self.gsub!(COMPLEX_WIKI_LINK) do
-      text, link = $1, $2
+      link, text = $1, $2
       '<a href="/%s">%s</a>' % [link.as_wiki_link, text]
     end
     self
