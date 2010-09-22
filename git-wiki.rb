c39f961f754c6fe51355e280cad1c0a8d6ba61a6
Tijn Schuurmans <tijn@soocial.com>
2010-09-23 00:55:59 +0200

    search on filenames

--- git-wiki.rb
+++ git-wiki.rb
@@ -137,6 +137,12 @@ end
 
 get '/a/search' do
   @search = params[:search]
+  pagenames = $repo.log.first.gtree.children.keys
+  @titles = {}
+  pagenames.select { |page| page.include? @search }.each do |page|
+    current_branch_sha1 = $repo.log.first
+    @titles["#{current_branch_sha1}:#{page}"] = page.map {|page| [0, page] }
+  end
   @grep = $repo.grep(@search)
   show :search, 'Search Results'
 end
