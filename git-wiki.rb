6efcfaae2fc78b1093060fb91f331a09134879c5
Tijn Schuurmans <tijn@soocial.com>
2010-11-06 00:09:42 +0100

    use <mark> tag and be more html5-like

--- git-wiki.rb
+++ git-wiki.rb
@@ -140,8 +140,12 @@ end
 
 get '/a/search' do
   @search = params[:search]
-  search_on_filename(@search)
+  @titles = search_on_filename(@search)
   @grep = $repo.grep(@search)
+  [@titles, @grep].each do |x|
+    puts x.inspect
+    x.values.each {|v| v.each { |w| w.last.gsub!(@search, "<mark>#{@search}</mark>") } }
+  end
   show :search, 'Search Results'
 end
 
@@ -173,12 +177,12 @@ end
 def search_on_filename(search)
   needle = search.as_wiki_link
   pagenames = $repo.log.first.gtree.children.keys # our haystack
-  @titles = {}
+  titles = {}
   pagenames.select { |page| page.include? needle }.each do |page|
     current_branch_sha1 = $repo.log.first
-    @titles["#{current_branch_sha1}:#{page}"] = page.map {|page| [0, page] }
+    titles["#{current_branch_sha1}:#{page}"] = page.map {|page| [0, page] }
   end
-  @titles
+  titles
 end
 
 def page_url(page)
